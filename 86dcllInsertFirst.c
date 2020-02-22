71854643fa730de453adcc9d17b8a6a58d0b7e23
adityaa71 <adityaanerao@yahoo.in>
2020-02-23 01:11:37 +0530

    till 88

---
+++ 86dcllInsertFirst.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	struct node* prev;
+	int data;
+	struct node* next;
+};
+
+struct node* newNode(int no);
+int insertFirst(struct node** src,struct node** tail,int no);
+void display(struct node**);
+struct node* findTail(struct node**);
+
+int main(){
+	int itr1,num,data;
+	struct node* head=NULL,*tail=NULL;
+	printf("Insert number of nodes to be created.\n");
+	scanf("%d",&num);
+	for(itr1=0;itr1<num;itr1++){
+		printf("Enter element %d: ",itr1+1);
+		scanf("%d",&data);
+		insertFirst(&head,&tail,data);
+	}
+	display(&head);
+	return 0;
+}
+
+int insertFirst(struct node** src,struct node** tail,int no){
+	struct node* temp1=*src;
+	struct node* temp2;
+	if(*src==NULL){
+		temp1=newNode(no);
+		temp1->prev=temp1->next=temp1;
+		*src=temp1;
+		*tail=temp1->prev;
+		return 0;
+	}else{
+		temp2=newNode(no);
+		temp2->next=temp1;
+		temp2->prev=temp1->prev;
+		temp1->prev=temp2;
+		temp2->prev->next=temp2;
+		*src=temp2;
+	}
+}
+
+struct node* findTail(struct node** head){
+	struct node* temp=*head;
+	if(*head==NULL){
+		printf("list is empty.\n");
+		return NULL;
+	}
+	while(temp->next!=*head){
+		temp=temp->next;
+	}
+	return temp;
+}
+
+void display(struct node** head){
+	struct node* temp=*head;
+	if(*head==NULL){
+		printf("List is empty.\n");
+		return;
+	}
+	printf("Output:\n");
+	while(temp->next!=*head){
+		printf("|%d|<=>",temp->data);
+		temp=temp->next;
+	}
+		printf("|%d|\n",temp->data);
+}
+
+struct node* newNode(int no){
+	struct node* new=(struct node*)malloc(sizeof(struct node));
+	new->data=no;
+	new->next=new->prev=NULL;
+	return new;
+}
+
---
+++ 88dcllInsertAtPos.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	struct node* prev;
+	int data;
+	struct node* next;
+};
+
+struct node* newNode(int no);
+int insertFirst(struct node** src,struct node** tail,int no);
+int insertLast(struct node** src,struct node** tail,int no);
+int insertAtPos(struct node**,struct node**,int,int);
+void display(struct node**);
+struct node* findTail(struct node**);
+
+int main(){
+	int itr1,num,data;
+	struct node* head=NULL,*tail=NULL;
+	printf("Insert number of nodes to be created.\n");
+	scanf("%d",&num);
+	for(itr1=0;itr1<num;itr1++){
+		printf("Enter element %d: ",itr1+1);
+		scanf("%d",&data);
+		insertLast(&head,&tail,data);
+	}
+	display(&head);
+	tail=findTail(&head);
+	printf("Enter number followed by position of node.\n");
+	scanf("%d %d",&data,&num);
+	insertAtPos(&head,&tail,num,data);
+	display(&head);
+	return 0;
+}
+
+int insertAtPos(struct node** src,struct node** tail,int pos,int no){
+	struct node* temp1=*src;
+	struct node* temp2=NULL;
+	if(*src==NULL){
+		printf("list is empty.\n");
+	}
+	if(pos==1){
+		insertFirst(src,tail,no);
+	}else{
+
+
+		while(pos>1){
+			temp1=temp1->next;
+			pos--;
+		}
+		temp2=newNode(no);
+		temp2->next=temp1;
+		temp2->prev=temp1->prev;
+		temp1->prev=temp2;
+		temp2->prev->next=temp2;
+
+	}
+}
+
+int insertLast(struct node** src,struct node** tail,int no){
+	struct node* temp1=*src;
+	struct node* Ttemp=*tail;
+	struct node* temp2;
+	if(*src==NULL){
+		temp2=newNode(no);
+		temp2->next=temp2->prev=temp2;
+		*src=temp2;
+		*tail=temp2->prev;	
+	}else{
+		temp2=newNode(no);
+		temp2->next=Ttemp->next;
+		Ttemp->next=temp2;
+		temp2->prev=Ttemp;
+		(*src)->prev=temp2;
+		*tail=temp2;
+	}
+}
+
+int insertFirst(struct node** src,struct node** tail,int no){
+	struct node* temp1=*src;
+	struct node* temp2;
+	if(*src==NULL){
+		temp1=newNode(no);
+		temp1->prev=temp1->next=temp1;
+		*src=temp1;
+		*tail=temp1->prev;
+		return 0;
+	}else{
+		temp2=newNode(no);
+		temp2->next=temp1;
+		temp2->prev=temp1->prev;
+		temp1->prev=temp2;
+		temp2->prev->next=temp2;
+		*src=temp2;
+		*tail=temp2->prev;
+	}
+}
+
+struct node* findTail(struct node** head){
+	struct node* temp=*head;
+	if(*head==NULL){
+		printf("list is empty.\n");
+		return NULL;
+	}
+	while(temp->next!=*head){
+		temp=temp->next;
+	}
+	return temp;
+}
+
+void display(struct node** head){
+	struct node* temp=*head;
+	if(*head==NULL){
+		printf("List is empty.\n");
+		return;
+	}
+	printf("Output:\n");
+	while(temp->next!=*head){
+		printf("|%d|<=>",temp->data);
+		temp=temp->next;
+	}
+	printf("|%d|\n",temp->data);
+}
+
+struct node* newNode(int no){
+	struct node* new=(struct node*)malloc(sizeof(struct node));
+	new->data=no;
+	new->next=new->prev=NULL;
+	return new;
+}
+
